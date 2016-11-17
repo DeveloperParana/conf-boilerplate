6f205e737551aa5c493677a5367a4a2846aebf00
Fernando Fabricio dos Santos <ffsantos@uolinc.com>
2016-11-17 13:47:09 -0200

    adicionando a ordem no schedule correto

--- docpad.js
+++ docpad.js
@@ -152,8 +152,12 @@ module.exports = {
     // The entire schedule
     schedule: [
       {
-        name: "Check-in / Breakfast",
+        name: "Credenciamento / Café",
         time: "8h00"
+      },
+      {
+        name: "Abertura",
+        time: "9h15"
       },
       {
         name: "Maujor",
@@ -171,37 +175,18 @@ module.exports = {
         }
       },
       {
-        name: "Edson Yanaga",
-        photo: "themes/devparana-conf-2016/img/speakers/yanaga.jpg",
-        bio: "",
-        company: "Red Hat",
+        name: "Eduardo Spaki",
+        photo: "themes/devparana-conf-2016/img/speakers/eduardo.jpg",
+        bio: "Arquiteto de Software, atua há 14 anos com desenvolvimento de tecnologias, tendo participado de projetos em diversos países. É especialista em softwares para a Internet e possui MBA em Gerência de Projetos. Já publicou livro e artigos na área de tecnologia e vem palestrando sobre carreira profissional, inovação e TI.",
+        company: "",
         link: {
-          href: "http://github.com/yanaga",
-          text: "@yanaga"
+          href: "https://twitter.com/eduardospaki",
+          text: "@eduardospaki"
         },
         presentation: {
-          title: "DevOps e Containers: como o Open Source está mudando a entrega de software",
-          description: "Nesse novo mundo com novos modelos econômicos surgindo nós temos que entregar software em produção cada vez melhor, mais rápido e de modo mais seguro. DevOps é o movimento atual para conseguirmos alcançar esse objetivo, e Containers é uma das tecnologias que fazem parte de toda essa inovação. Venha aprender como Containers e diversas ferramentas Open Source estão habilitando o movimento de DevOps para entregar software mais rápido em produção.",
-          time: "11h00"
-        }
-      },
-      {
-        name: "Lunch",
-        time: "12h00"
-      },
-      {
-        name: "Jaydson Gomes",
-        photo: "themes/devparana-conf-2016/img/speakers/jaydson.jpg",
-        bio: "Jaydson Gomes tem +de 12 anos de experiência em desenvolvimento Web.",
-        company: "Nasc",
-        link: {
-          href: "http://twitter.com/jaydson",
-          text: "@jaydson"
-        },
-        presentation: {
-          title: "Como ser um bom dev FrontEnd em 2017",
-          description: "Nos últimos o chamado desenvolvimento FrontEnd nasceu e cresceu de tal maneira que, para muitos, atingiu demasiada complexidade. De fato, a evolução do JavaScript, do HTML e do CSS, resultou no nascimento de uma variedade de novos conceitos, ferramentas, bibliotecas e frameworks. Como se manter atualizado e relevante em um ambiente tão volátil? Como ser um bom desenvolvedor FrontEnd? Nesta palestra Jaydson irá mostrar sua visão do mundo FrontEnd e o que é necessário para se tornar um bom desenvolvedor FrontEnd em 2017.",
-          time: "13h00"
+          title: "Progressive Web Apps, será essa a nova tendência!?",
+          description: "A tecnologia tem suas modas e vive se reinventando. Já tivemos a web, web 2.0, mobile, apps... Mas o que será que virá a seguir? Que tal conhecer um pouco do que os Progressive Web Apps tem para oferecer à este mundo cada vez mais conectado.",
+          time: "10h20"
         }
       },
       {
@@ -215,13 +200,17 @@ module.exports = {
         },
         presentation: {
           title: "REACT",
-          description: "Uma introdução ao framework e demonstração das possibilidades de sua utilização.",
-          time: "09h50"
+          description: "Algumas demonstrações da flexibilidade e das possibilidades de sua utilização.",
+          time: "11h10"
         }
       },
       {
-        name: "Coffee-break",
-        time: "15h00"
+        name: "Almoço",
+        time: "12h00"
+      },
+      {
+        name: "Abertura e sorteios",
+        time: "13h20"
       },
       {
         name: "Fábio Rogério SJ",
@@ -235,37 +224,22 @@ module.exports = {
         presentation: {
           title: "Desenvolvimento híbrido para mobile e desktop",
           description: "Passamos por muitas mudanças na evolução das plataformas de interação entre sistemas e usuários. Quando migramos do desktop para web um mundo de possibilidades, para os desenvolvedores, foi criado e hoje vivemos a expansão de aplicações web para o desktop, mas o que acontece com o mundo de possibilidades? Desenvolver aplicações híbridas é trazer todo o poder das tecnologias web para o desktop e mobile.",
-          time: ""
+          time: "13h30"
         }
       },
       {
-        name: "Eduardo Spaki",
-        photo: "themes/devparana-conf-2016/img/speakers/eduardo.jpg",
-        bio: "Arquiteto de Software, atua há 14 anos com desenvolvimento de tecnologias, tendo participado de projetos em diversos países. É especialista em softwares para a Internet e possui MBA em Gerência de Projetos. Já publicou livro e artigos na área de tecnologia e vem palestrando sobre carreira profissional, inovação e TI.",
-        company: "",
+        name: "Jaydson Gomes",
+        photo: "themes/devparana-conf-2016/img/speakers/jaydson.jpg",
+        bio: "Jaydson Gomes tem +de 12 anos de experiência em desenvolvimento Web.",
+        company: "Nasc",
         link: {
-          href: "https://twitter.com/eduardospaki",
-          text: "@eduardospaki"
+          href: "http://twitter.com/jaydson",
+          text: "@jaydson"
         },
         presentation: {
-          title: "Progressive Web Apps, será essa a nova tendência!?",
-          description: "A tecnologia tem suas modas e vive se reinventando. Já tivemos a web, web 2.0, mobile, apps... Mas o que será que virá a seguir? Que tal conhecer um pouco do que os Progressive Web Apps tem para oferecer à este mundo cada vez mais conectado.",
-          time: ""
-        }
-      },
-      {
-        name: "Anderson M Fernandes",
-        photo: "themes/devparana-conf-2016/img/speakers/burnes.jpg",
-        bio: "Mestre em Informática pela UTFPR, Desenvolvedor Web desde 1999, Professor de Graduação e Pós-Graduação, Palestrante e degustador de Bacon. Atua ainda como sócio, desenvolvedor e gestor de e-commerce na Uniti, onde mantem parcerias com as lojas Vida de Programador, Vida de Suporte, Bugginho Developer e o Compra Canibal dos Irmãos Piologo.",
-        company: "Faculdade Alfa",
-        link: {
-          href: "https://twitter.com/professorburnes",
-          text: "@ProfessorBurnes"
-        },
-        presentation: {
-          title: "Realidade: Virtual ou Aumentada?",
-          description: "Palestra sobre os temas Realidade Virtual e Aumentada, diferenças e aplicações para Web e Dispositivos Móveis. Nesta palestra são apresentados ao público de maneira prática o funcionamento, utilizando um Óculos de RV e o Sphero com um game com RA (https://www.youtube.com/watch?v=UPn3jVGQw68), além de discorrer sobre o sucesso do Pokemon Go e ferramentas de desenvolvimento Web.",
-          time: ""
+          title: "Como ser um bom dev FrontEnd em 2017",
+          description: "Nos últimos o chamado desenvolvimento FrontEnd nasceu e cresceu de tal maneira que, para muitos, atingiu demasiada complexidade. De fato, a evolução do JavaScript, do HTML e do CSS, resultou no nascimento de uma variedade de novos conceitos, ferramentas, bibliotecas e frameworks. Como se manter atualizado e relevante em um ambiente tão volátil? Como ser um bom desenvolvedor FrontEnd? Nesta palestra Jaydson irá mostrar sua visão do mundo FrontEnd e o que é necessário para se tornar um bom desenvolvedor FrontEnd em 2017.",
+          time: "15h10"
         }
       },
       {
@@ -282,6 +256,44 @@ module.exports = {
           description: "A era de grandes sistemas monolíticos esta acabando, sua manutenção, sua homogeneidade em tecnologias, suas necessidades específicas não tem mais espaço no mundo heterogêneo atual. Com a utilização de microserviços é possível criar soluções realmente reutulizáveis, heterogêneas e independentes de tecnologias. Esta paletra tem o objetivo de descrever as tecnologias de microserviços mostrando os conceitos e implementações da NetFlix utilizando como exemplo Spring e Java .",
           time: ""
         }
+      },
+      {
+        name: "Coffee Break",
+        time: "16h00"
+      },
+      {
+        name: "Anderson M Fernandes",
+        photo: "themes/devparana-conf-2016/img/speakers/burnes.jpg",
+        bio: "Mestre em Informática pela UTFPR, Desenvolvedor Web desde 1999, Professor de Graduação e Pós-Graduação, Palestrante e degustador de Bacon. Atua ainda como sócio, desenvolvedor e gestor de e-commerce na Uniti, onde mantem parcerias com as lojas Vida de Programador, Vida de Suporte, Bugginho Developer e o Compra Canibal dos Irmãos Piologo.",
+        company: "Faculdade Alfa",
+        link: {
+          href: "https://twitter.com/professorburnes",
+          text: "@ProfessorBurnes"
+        },
+        presentation: {
+          title: "Realidade: Virtual ou Aumentada?",
+          description: "Palestra sobre os temas Realidade Virtual e Aumentada, diferenças e aplicações para Web e Dispositivos Móveis. Nesta palestra são apresentados ao público de maneira prática o funcionamento, utilizando um Óculos de RV e o Sphero com um game com RA (https://www.youtube.com/watch?v=UPn3jVGQw68), além de discorrer sobre o sucesso do Pokemon Go e ferramentas de desenvolvimento Web.",
+          time: "16h30"
+        }
+      },
+      {
+        name: "Edson Yanaga",
+        photo: "themes/devparana-conf-2016/img/speakers/yanaga.jpg",
+        bio: "",
+        company: "Red Hat",
+        link: {
+          href: "http://github.com/yanaga",
+          text: "@yanaga"
+        },
+        presentation: {
+          title: "DevOps e Containers: como o Open Source está mudando a entrega de software",
+          description: "Nesse novo mundo com novos modelos econômicos surgindo nós temos que entregar software em produção cada vez melhor, mais rápido e de modo mais seguro. DevOps é o movimento atual para conseguirmos alcançar esse objetivo, e Containers é uma das tecnologias que fazem parte de toda essa inovação. Venha aprender como Containers e diversas ferramentas Open Source estão habilitando o movimento de DevOps para entregar software mais rápido em produção.",
+          time: "17h30"
+        }
+      },
+      {
+        name: "Encerramento",
+        time: "18h30"
       },
     ],
 
