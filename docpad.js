eb9344df94bec4aeb691a45628f21becd3a4c6d9
Junior Conte <juniorcnt@gmail.com>
2017-07-10 23:13:28 -0300

    Inclusão da talk e agenda com o Yanaga

--- docpad.js
+++ docpad.js
@@ -60,13 +60,13 @@ module.exports = {
     // The speakers
     speakers: [
       {
-        name: "Alê Borba",
-        photo: "themes/devparana-conf-2017/img/speakers/aleborba.jpg",
+        name: "Edson Yanaga",
+        photo: "themes/devparana-conf-2017/img/speakers/yanaga.jpg",
         bio: "",
-        company: "Google",
+        company: "Red Hat",
         link: {
-          href: "https://github.com/aleborba",
-          text: "@aleborba"
+          href: "https://www.linkedin.com/in/yanaga",
+          text: "@yanaga"
         }
       },
       {
@@ -152,33 +152,18 @@ module.exports = {
         time: "8h30"
       },
       {
-        name: "Alê Borba",
-        photo: "themes/devparana-conf-2017/img/speakers/aleborba.jpg",
-        bio: "",
-        company: "Google",
+        name: "Leonardo Saraiva",
+        photo: "themes/devparana-conf-2017/img/speakers/vyper.jpg",
+        bio: "Co-fundador do GURU-PR e do WSO2 Brasil! Organizador do Tech Day, que ocorre anualmente em Curitiba. Desenvolvedor louco por tecnologia, atualmente surfando na onda de ruby, rails, hanami, go, grpc, docker, microserviços e kubernetes. Mas com experiência em sinatra, python (django), java, c# e um pouco de tudo, mas já não tão ativamente.",
+        company: "Parafuzo",
         link: {
-          href: "https://github.com/aleborba",
-          text: "@aleborba"
+          href: "https://github.com/vyper",
+          text: "@vyper"
         },
         presentation: {
-          title: "",
-          description: "Em breve",
+          title: "Liberdade e escalabilidade: Microserviços, Kubernetes, gRPC e Docker",
+          description: "Usando Kubernetes e Docker é possível entregar uma arquitetura de microserviços, multi cloud e encontrar a felicidade em forma de escalabilidade e liberdade, sem ficar amarrado e dependente das 'mágicas' de um PaaS.",
           time: "08h40"
-        }
-      },
-      {
-        name: "Raphael Amorim",
-        photo: "themes/devparana-conf-2017/img/speakers/raphael.jpg",
-        bio: "Entusiasta da open web e software livre. Software Developer na globo.com e membro da JS foundation e jQuery team. Já trabalhou como desenvolvedor em empresas como Videolog e Petrobras. Em 2015, foi um dos desenvolvedores de software mais notáveis no GitHub, tendo feito contribuições significativas para projetos de código aberto por 500 dias seguidos. Orador em mais de vinte conferências dando palestras sobre tecnologias web como JavaScript / ES6, Web Performance, React e Canvas / WebGL.",
-        company: "Globo.com",
-        link: {
-          href: "https://github.com/raphamorim",
-          text: "@raphamorim"
-        },
-        presentation: {
-          title: "Merging Products with Components",
-          description: "Nesta palestra, veremos o case da plataforma de vídeos da globo.com, onde em diversos produtos com times diferentes havia a necessidade de unificar tanto componentes visuais quanto fonte de dados (que são baseadas em APIs diferentes, de produtos diferentes [globoplay, globosatplay, sportv, telecine…]), logo surgiu uma plataforma de componentes dentro da área de vídeos da globo.com. A ideia é contar sobre as escolhas de arquitetura e de nível técnico voltadas para atender diversos produtos, times, fontes de dados.",
-          time: "09h30"
         }
       },
       {
@@ -193,21 +178,36 @@ module.exports = {
         presentation: {
           title: "Descomplicando o Docker",
           description: "Vamos abordar de maneira muito leve e divertida tudo o que você precisa saber para criar, administrar e escalar containers utilizando Docker! Traga seu notebook, pois como sempre será prático! #VAIIII",
+          time: "09h30"
+        }
+      },
+      {
+        name: "Rodrigo Nonose",
+        photo: "themes/devparana-conf-2017/img/speakers/nonose.jpg",
+        bio: "Desenvolvedor de Software na Digital Natives, atualmente usando Elixir",
+        company: "Digital Natives",
+        link: {
+          href: "https://github.com/rhnonose",
+          text: "@rhnonose"
+        },
+        presentation: {
+          title: "Programação Funcional na web com Elixir e Elm",
+          description: "O paradigma funcional está cada vez mais em vista recentemente e com boa razão: diversas linguagens e frameworks têm trazido um ponto de vista diferente para se resolver os problemas inerentes do desenvolvimento web. Nesta apresentação, pretende-se ilustrar conceitualmente como o as linguagens Elixir (backend) e Elm (frontend) resolvem os problemas mais comuns de uma maneira elegante e compreensível.",
           time: "10h20"
         }
       },
       {
-        name: "Leonardo Saraiva",
-        photo: "themes/devparana-conf-2017/img/speakers/vyper.jpg",
-        bio: "Co-fundador do GURU-PR e do WSO2 Brasil! Organizador do Tech Day, que ocorre anualmente em Curitiba. Desenvolvedor louco por tecnologia, atualmente surfando na onda de ruby, rails, hanami, go, grpc, docker, microserviços e kubernetes. Mas com experiência em sinatra, python (django), java, c# e um pouco de tudo, mas já não tão ativamente.",
-        company: "Parafuzo",
+        name: "Edson Yanaga",
+        photo: "themes/devparana-conf-2017/img/speakers/yanaga.jpg",
+        bio: "Edson atua na Red Hat como Diretor de Experiência do Desenvolvedor. É Java Champion, Microsoft MVP e autor reconhecido, além de palestrante frequente em eventos internacionais, discutindo Java, Cloud Computing, DevOps, Microservices, Domain-Driven Design e Software Craftsmanship",
+        company: "Red Hat",
         link: {
-          href: "https://github.com/vyper",
-          text: "@vyper"
+          href: "https://www.linkedin.com/in/yanaga",
+          text: "@yanaga"
         },
         presentation: {
-          title: "Liberdade e escalabilidade: Microserviços, Kubernetes, gRPC e Docker",
-          description: "Usando Kubernetes e Docker é possível entregar uma arquitetura de microserviços, multi cloud e encontrar a felicidade em forma de escalabilidade e liberdade, sem ficar amarrado e dependente das 'mágicas' de um PaaS.",
+          title: "Dados e Microservices: lidando com bancos legados na era de serviços distribuídos",
+          description: "Nas arquiteturas de microservices, com suas múltiplas partes distribuídas e interconectadas, não podemos permitir que o downtime gerado por um único componente derrube o sistema inteiro. Isso é especialmente delicado em sistemas usando bancos legados, originados de arquiteturas monolíticas. No legado um modelo dominante é o baseado em transações, consistência forte e CRUD. E para garantir integridade e consistência sem gerar downtime, é necessário rever esses conceitos. Nesta palestra discutiremos consistência eventual e consistência forte, CQRS e Event Sourcing, e como essas técnicas se relacionam e se aplicam em estratégias de integração e evolução de dados relacionais, no contexto de microservices. Vamos explorar Views, Materialized Views, Mirror Tables,Streaming e Data Virtualization, e como seu uso viabiliza arquiteturas eficazes baseadas em microservices, partindo de um legado monolítico e de BDs relacionais tradicionais.",
           time: "11h10"
         }
       },
@@ -231,17 +231,17 @@ module.exports = {
         }
       },
       {
-        name: "Rodrigo Nonose",
-        photo: "themes/devparana-conf-2017/img/speakers/nonose.jpg",
-        bio: "Desenvolvedor de Software na Digital Natives, atualmente usando Elixir",
-        company: "Digital Natives",
+        name: "Raphael Amorim",
+        photo: "themes/devparana-conf-2017/img/speakers/raphael.jpg",
+        bio: "Entusiasta da open web e software livre. Software Developer na globo.com e membro da JS foundation e jQuery team. Já trabalhou como desenvolvedor em empresas como Videolog e Petrobras. Em 2015, foi um dos desenvolvedores de software mais notáveis no GitHub, tendo feito contribuições significativas para projetos de código aberto por 500 dias seguidos. Orador em mais de vinte conferências dando palestras sobre tecnologias web como JavaScript / ES6, Web Performance, React e Canvas / WebGL.",
+        company: "Globo.com",
         link: {
-          href: "https://github.com/rhnonose",
-          text: "@rhnonose"
+          href: "https://github.com/raphamorim",
+          text: "@raphamorim"
         },
         presentation: {
-          title: "Programação Funcional na web com Elixir e Elm",
-          description: "O paradigma funcional está cada vez mais em vista recentemente e com boa razão: diversas linguagens e frameworks têm trazido um ponto de vista diferente para se resolver os problemas inerentes do desenvolvimento web. Nesta apresentação, pretende-se ilustrar conceitualmente como o as linguagens Elixir (backend) e Elm (frontend) resolvem os problemas mais comuns de uma maneira elegante e compreensível.",
+          title: "Merging Products with Components",
+          description: "Nesta palestra, veremos o case da plataforma de vídeos da globo.com, onde em diversos produtos com times diferentes havia a necessidade de unificar tanto componentes visuais quanto fonte de dados (que são baseadas em APIs diferentes, de produtos diferentes [globoplay, globosatplay, sportv, telecine…]), logo surgiu uma plataforma de componentes dentro da área de vídeos da globo.com. A ideia é contar sobre as escolhas de arquitetura e de nível técnico voltadas para atender diversos produtos, times, fontes de dados.",
           time: "14h50"
         }
       },
@@ -276,12 +276,12 @@ module.exports = {
         presentation: {
           title: "",
           description: "Em breve",
-          time: "17h10"
+          time: "17h30"
         }
       },
       {
         name: "Encerramento e Sorteios",
-        time: "18h00"
+        time: "18h20"
       },
       {
         name: "Happy Hour e Networking",
@@ -292,9 +292,9 @@ module.exports = {
     // List of Sponsors
     sponsors: [
       {
-        name: "Google Developers",
-        logo: "themes/devparana-conf-2017/img/google-developers.svg",
-        url: "https://developers.google.com"
+        name: "Red Hat",
+        logo: "themes/devparana-conf-2017/img/redhat.svg",
+        url: "https://www.redhat.com/pt-br"
       },
       {
         name: "Tomitribe",
